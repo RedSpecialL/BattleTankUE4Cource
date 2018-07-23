e081a698b77e391997efc03ef0ba7a8ad78ecbc1
RedSpecialL <rdspcll@gmail.com>
2018-07-23 22:29:39 +0300

    Added zeroing of z component of rightvector to avoid strange behavior in the air.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -13,7 +13,6 @@ void UTankTrack::SetThrottle(float Throttle)
 	
 	FVector ForceToApply = GetForwardVector() * Throttle * MaxDrivingForce;
 	FVector ForceLocation = GetComponentLocation();
-	UE_LOG(LogTemp, Warning, TEXT("%s applaying force: %s at %s"), *GetName(), *ForceToApply.ToString(), *ForceLocation.ToString())
 	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceToApply, ForceLocation);
 }
@@ -21,10 +20,11 @@ void UTankTrack::SetThrottle(float Throttle)
 void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	float SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
-	FVector CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	FVector RightVector = GetRightVector();
+	RightVector.Z = 0;
+	float SlippageSpeed = FVector::DotProduct(RightVector, GetComponentVelocity());
+	FVector CorrectionAcceleration = -SlippageSpeed / DeltaTime * RightVector;
 	UStaticMeshComponent* TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
-	FVector CorrctionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Two tracks.
-	TankRoot->AddForce(CorrctionForce);
+	FVector CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Two tracks.
+	TankRoot->AddForce(CorrectionForce);
 } 
